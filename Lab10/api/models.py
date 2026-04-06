e77456333427a55bcb0315c55115bef030e991a7
Adik2005 <147049722+Adik2005@users.noreply.github.com>
2026-04-06 19:19:55 +0500

    Lab10

---
+++ Lab10/api/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "Category"
+        verbose_name_plural = "Categories"
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True, null=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='products')
+    stock = models.PositiveIntegerField(default=0)
+    available = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.name
